6123546e32a7585bfbec955c016bf265fea32d9b
nlacerdagabriel <devlacerdagabriel@hotmail.com>
2023-01-10 10:45:37 -0300

    changing baseURL path of backend

--- src/services/backendService.ts
+++ src/services/backendService.ts
@@ -2,7 +2,7 @@ import axios from "axios"
 import { IWordSpelling } from "../interfaces/wordInterfaces"
 
 const api = axios.create({
-  baseURL: "http://backend-transcription.s3-website-us-east-1.amazonaws.com/custom-word",
+  baseURL: "https://backend-transcripton.herokuapp.com/custom-word",
 })
 
 export const getAllWords = async () => {
